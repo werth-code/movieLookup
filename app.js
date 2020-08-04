5f1d655062649efc29149aa0e3dcfb9d7d15e52e
Matt Werth <matthewwerth@gmail.com>
2020-08-04 09:21:16 -0400

    Added Movie API

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const app = express()
 let port = 3000
+const request = require('request')
+
+app.get("/results", (req, res)=>{
+    request("http://www.omdbapi.com/?s=fugitive&apikey=thewdb", (error, response, body)=>{
+        if(!error && response.statusCode === 200) {
+            let results = JSON.parse(body);
+            res.send(results["Search"]);
+        }
+    });
+})
 
 
 app.listen(port, () => console.log(`App listening at http://localhost:${port}`))
