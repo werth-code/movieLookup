775bf1a93c421f82b849e30dd0a2c77da5e03dd1
Matt Werth <matthewwerth@gmail.com>
2020-08-04 10:18:51 -0400

    Added Basic Movie Search Capability

--- app.js
+++ app.js
@@ -3,13 +3,22 @@ const app = express()
 let port = 3000
 const request = require('request')
 
+app.set("view engine", 'ejs')
+
 app.get("/results", (req, res)=>{
-    request("http://www.omdbapi.com/?s=fugitive&apikey=thewdb", (error, response, body)=>{
+    let query = req.query.search
+    let url = "http://www.omdbapi.com/?s="+query+"&apikey=thewdb";
+    
+    request(url, (error, response, body)=>{
         if(!error && response.statusCode === 200) {
-            let results = JSON.parse(body);
-            res.send(results["Search"]);
+            let data = JSON.parse(body);
+            res.render('results', {data: data});
         }
     });
+})
+
+app.get("/", (req, res) => {
+    res.render('search')
 })
 
 
